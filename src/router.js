1684a966d2a07468b31a693f3ad3449f4c698e2b
tessabill <t.billens@campus.tu-berlin.de>
2024-09-02 15:50:42 +0200

    routing

--- src/router.js
+++ src/router.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import About from '@/views/About.vue';
 import CV from '@/views/CV.vue';
 import Projects from '@/views/Projects.vue';
@@ -13,7 +13,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory('/portfolio/'),
+  history: createWebHashHistory('/portfolio/'),
   routes,
 });
 
